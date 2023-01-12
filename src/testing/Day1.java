733f6ad4bc38057bb834b114e777b91f2afa3c07
ndusmzole <nduduzosmsomi@gmail.com>
2023-01-12 17:15:29 +0200

    my first commit

---
+++ src/testing/Day1.java
@@ -0,0 +1,16 @@
+package testing;
+
+import org.testng.annotations.Test;
+
+public class Day1 {
+	
+	@Test
+	public void demo() {
+		System.out.println("Hello TestNg");
+	}
+	@Test
+	public void lemon() {
+		System.out.println("Helo 2nd");
+	}
+
+}
